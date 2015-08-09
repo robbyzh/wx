27a01a94fba160a58e8f25481e713fd05dd629a7
zhaogang <zg3407@gmail.com>
2015-08-09 16:45:41 +0800

    add  chat view

---
+++ app/src/main/java/com/telchina/wx/Msg.java
@@ -0,0 +1,34 @@
+package com.telchina.wx;
+
+/**
+ * Created by zg on 2015/8/9.
+ */
+public class Msg {
+
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SEND = 1;
+
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
---
+++ app/src/main/java/com/telchina/wx/News.java
@@ -0,0 +1,9 @@
+package com.telchina.wx;
+
+/**
+ * Created by zg on 2015/8/9.
+ */
+public class News {
+
+
+}
